ba932ea0992c01577428f726f685407ad3fd4bb2
wrench1815 <hardeepkumar1815@gmail.com>
2021-02-10 18:09:46 +0530

    update settings add url maps

--- mainpages/urls.py
+++ mainpages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home, about, contactMe
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
     path('contact/', contactMe, name='contactMe'),
     path('about/', about, name='about'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
