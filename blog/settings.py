9e8607d4acee1b1f4320e3c5abddeab9dc7dadb8
wrench1815 <hardeepkumar1815@gmail.com>
2021-04-06 18:00:44 +0530

    update settings

--- blog/settings.py
+++ blog/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = BASE_DIR / 'statics'
 # For Media Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
