4fe87a1831c4136d6d896d604576b14422ca7315
wrench1815 <hardeepkumar1815@gmail.com>
2021-07-14 14:30:49 +0530

    Fix Blog List Page Add Blog Detail system(single Post)

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import BlogPostsListView
+from .views import BlogPostsListView, BlogPostsDetailView
 
 urlpatterns = [
     path('', BlogPostsListView.as_view(), name='blogs'),
+    path('<slug:slug>-<int:pk>/',
+         BlogPostsDetailView.as_view(),
+         name='blogs-post-detail')
 ]
