d803f0fd22406080cc2f799e14933c795aa72f5a
wrench1815 <hardeepkumar1815@gmail.com>
2021-07-16 16:31:46 +0530

    add ckeditor to changelogs model

--- mainpages/models.py
+++ mainpages/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-# Changelog Models
 
 
+# Changelog Model
 class ChangelogsModel(models.Model):
     """Model definition for ChangelogsModel."""
 
     version_title = models.CharField(max_length=100)
-    version_description = models.TextField()
+    version_description = RichTextField()
 
     class Meta:
         """Meta definition for changelogsModel."""
