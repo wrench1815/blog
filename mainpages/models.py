7c0393ea3384a15f05a42e68ba3607310b6ad094
wrench1815 <hardeepkumar1815@gmail.com>
2021-07-16 15:20:46 +0530

    add changelogs system to mainpages

--- mainpages/models.py
+++ mainpages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+# Changelog Models
 
-# Create your models here.
+
+class ChangelogsModel(models.Model):
+    """Model definition for ChangelogsModel."""
+
+    version_title = models.CharField(max_length=100)
+    version_description = models.TextField()
+
+    class Meta:
+        """Meta definition for changelogsModel."""
+
+        verbose_name = 'Changelog'
+        verbose_name_plural = 'Changelogs'
+
+    def __str__(self):
+        """Unicode representation of changelogsModel."""
+        return self.version_title
